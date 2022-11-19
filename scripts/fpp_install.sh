aaad4cd762dc23b0e9335ec8ef9ba16193561ba7
jeffbmiller <jeffmiller11@hotmail.com>
2022-11-19 16:05:18 -0600

    Changes

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
-
-# fpp-plugin-Template install script
-
+pushd $(dirname $(which $0))
+target_PWD=$(readlink -f .)
+/opt/fpp/scripts/update_plugin ${target_PWD##*/}
+echo ; echo “Please reboot fppd.” ; echo
+. /opt/fpp/scripts/common
+setSetting rebootFlag 1
+popd
